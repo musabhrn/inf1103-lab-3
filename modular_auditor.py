e7e8d7e5b268348fed2aed0c7e6d1d3e6a3aacf2
Musa Haron <2605752@sit.singaporetech.edu.sg>
2026-09-18 15:06:21 +0800

    Add function get_valid_input for input validation

--- modular_auditor.py
+++ modular_auditor.py
@@ -1,26 +1,36 @@
+def get_valid_input():
+    global failed_entries
+    while True:
+        user_input = input("Enter stock quantity (or 'quit' to exit): ").strip()
+
+        if user_input.lower() == "quit":
+            return "quit"
+
+        if not user_input.isdigit():
+            print("Error. Invalid input. Please enter a non-negative number.")
+            failed_entries += 1
+        else:
+            return int(user_input)
+
+ # Main
 inventory = 0
 failed_entries = 0
 
 while True:
-    user_input = input("Enter stock quantity (or 'quit' to exit): ").strip()
+    result = get_valid_input()
 
-    if user_input.lower() == "quit":
+    if result == "quit":
         break
 
-    if not user_input.isdigit():
-        print("Error. Invalid input. Please enter a non-negative number.")
-        failed_entries += 1
-        continue
+#     stock_quantity = int(user_input)
 
-    stock_quantity = int(user_input)
+#     inventory += stock_quantity
 
-    inventory += stock_quantity
-
-    if inventory > 500:
-        print("Alert! Inventory total exceeds 500 units.")
-        break
+#     if inventory > 500:
+#         print("Alert! Inventory total exceeds 500 units.")
+#         break
 
 
-print("-------------------------------")
-print(f"Total Units Processed: {inventory}")    
-print(f"Number of Failed/Rejected Entries: {failed_entries}")
+# print("-------------------------------")
+# print(f"Total Units Processed: {inventory}")    
+# print(f"Number of Failed/Rejected Entries: {failed_entries}")
